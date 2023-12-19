61ce510164d717d85ba911e4d03b08adc9c02431
Morgan West <mewwes@gmail.com>
2023-12-18 18:02:19 -0800

    tailwind

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function UserButton() {
   return (
-    <div className="bg-blue-500 text-black p-4">UserButton</div>
+    <div className="bg-blue-500 text-orange-700 p-4">UserButton</div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}', // Path to all files in the 'pages' directory
+    './components/**/*.{js,jsx,ts,tsx}', // Path to all files in the 'components' directory
+    './app/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './layouts/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
