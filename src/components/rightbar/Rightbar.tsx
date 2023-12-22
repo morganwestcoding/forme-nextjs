9dee40359152f29e08164a731a4004fbed35e918
Morgan West <mewwes@gmail.com>
2023-12-21 16:26:25 -0800

    ui updates

--- src/components/rightbar/Rightbar.tsx
+++ src/components/rightbar/Rightbar.tsx
@@ -2,8 +2,8 @@ import Online from "./Online";
 
 export default function Rightbar() {
     return (
-      <div className="bg-transparent pt-8 pl-16">
-      <div className="h-screen rounded-lg bg-white shadow-md dark:shadow-lg dark:border dark:border-gray-600 p-10">
+      <div className="flex justify-end bg-transparent pt-8">
+      <div className="w-10/12 h-screen rounded-lg bg-white shadow-md dark:shadow-lg dark:border dark:border-gray-600 p-10">
       rightbar
 
       </div>
