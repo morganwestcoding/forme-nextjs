ae4784ed10ce65c11f08716af91e8f583df99b05
Morgan West <mewwes@gmail.com>
2025-01-01 23:46:46 -0800

    rounded avatars

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar: React.FC<AvatarProps> = ({ src, isSidebar }) => {
   const imageSrc = src || defaultImage;
 
   return (
-    <div className={`relative ${isSidebar ? 'w-10 h-10 rounded-lg' : 'w-11 h-11'} rounded-full overflow-hidden`}>
+    <div className={`relative ${isSidebar ? 'w-10 h-10 rounded-lg' : 'w-11 h-11 rounded-full'} overflow-hidden`}>
       <Image
         layout="fill"
         objectFit="cover" 
