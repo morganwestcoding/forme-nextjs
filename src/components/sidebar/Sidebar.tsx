2cf81fe116ac88c58cecb67a93a05bad215cc2ca
Morgan West <mewwes@gmail.com>
2025-03-05 22:45:38 -0800

    icon resolve

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -184,7 +184,7 @@ const Sidebar: React.FC<SidebarProps> = ({
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   color={selectedButton === 'explore' ? "#ffffff" : "#71717A"} 
-                  className="group-hover:stroke-white lucide lucide-rocket"
+                  className="group-hover:text-white"
                 >
                   <path d="M9.49811 15L16.9981 7.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                   <path d="M8.00634 7.67888L15.327 4.21881C18.3688 2.78111 19.8897 2.06226 20.8598 2.78341C21.8299 3.50455 21.5527 5.14799 20.9984 8.43486L20.0435 14.0968C19.6811 16.246 19.4998 17.3205 18.6989 17.7891C17.8979 18.2577 16.8574 17.8978 14.7765 17.178L8.41077 14.9762C4.51917 13.6301 2.57337 12.9571 2.50019 11.6365C2.427 10.3159 4.28678 9.43692 8.00634 7.67888Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
