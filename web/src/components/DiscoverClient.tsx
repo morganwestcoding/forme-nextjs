191d2d0d4a3166e9986c0e24694d5c5cc8a275b8
Morgan West <mewwes@gmail.com>
2026-04-13 13:55:30 -0700

    Update DiscoverClient.tsx

--- web/src/components/DiscoverClient.tsx
+++ web/src/components/DiscoverClient.tsx
@@ -245,8 +245,8 @@ const DiscoverClient: React.FC<DiscoverClientProps> = ({
 
   // Responsive grid - adds 1 column when sidebar is collapsed
   const gridColsClass = isSidebarCollapsed
-    ? 'grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
-    : 'grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4';
+    ? 'grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'
+    : 'grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3';
 
   const filterInfo = useMemo(() => {
     const categoryIsActive = currentCategories.length > 0;
